10d92762df70443959394eb0d3bdf45070e2e0ea
Elemas <pierre.christaller@gmail.com>
2021-09-26 23:52:32 +0200

    separating events

--- Controller/CommandHandler.cs
+++ Controller/CommandHandler.cs
@@ -11,50 +11,27 @@ namespace StageBot.Controller
 {
 	public class CommandHandler
 	{
-		private const string COMMAND_MARK = "!";
-
 		private readonly DiscordSocketClient _client;
 		private readonly CommandService _command;
 		private readonly IServiceProvider _services;
+		private readonly SocketClientEvents _socketClientEvents;
 
 		public CommandHandler(IServiceProvider services, DiscordSocketClient client, CommandService commands)
 		{
 			_services = services;
 			_client = client;
 			_command = commands;
+			_socketClientEvents = new SocketClientEvents(_services, _client, _command);
 		}
 
 		public async Task InitializeAsync()
 		{
-			_client.MessageReceived += OnMessageReceivedAsync;
-			_client.MessageUpdated += async (before, after, channel) => { await OnMessageUpdated(before, after, channel); };
+			_client.MessageReceived += _socketClientEvents.OnMessageReceivedAsync;
+			_client.MessageUpdated += _socketClientEvents.OnMessageUpdated;
 			await _command.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
 			_command.CommandExecuted += OnCommandExecutedAsync;
-			_client.MessageCommandExecuted += OnMessageCommandExecuted;
-			_client.UserCommandExecuted += OnUserCommandExecuted;
-		}
-
-		private Task OnMessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
-		{
-			//// If the message was not in the cache, downloading it will result in getting a copy of `after`.
-			//var message = await before.GetOrDownloadAsync();
-			//Console.WriteLine($"{message} -> {after}");
-
-			return Task.CompletedTask;
-		}
-
-		// todo : maybe remove these events
-
-		private Task OnMessageCommandExecuted(SocketMessageCommand command)
-		{
-			LogService.Info(nameof(OnMessageCommandExecuted), LogService.SUCCESS);
-			return Task.CompletedTask;
-		}
-
-		private Task OnUserCommandExecuted(SocketUserCommand command)
-		{
-			LogService.Info(nameof(OnUserCommandExecuted), LogService.SUCCESS);
-			return Task.CompletedTask;
+			_client.MessageCommandExecuted += _socketClientEvents.OnMessageCommandExecuted;
+			_client.UserCommandExecuted += _socketClientEvents.OnUserCommandExecuted;
 		}
 
 		private Task OnCommandExecutedAsync(Optional<CommandInfo> commandInfo, ICommandContext context, IResult result)
@@ -67,53 +44,6 @@ namespace StageBot.Controller
 			} else
 				LogService.Warn(nameof(OnCommandExecutedAsync), LogService.MISSING_CMD);
 			return Task.CompletedTask;
-		}
-
-		private async Task OnMessageReceivedAsync(SocketMessage messageParam)
-		{
-			// don't process the command if it was a system message
-			if (!(messageParam is SocketUserMessage message))
-				return;
-
-			// index of start of actual message
-			int argPos = 0;
-			// filter messages
-			if (!message.HasStringPrefix(COMMAND_MARK, ref argPos)
-				|| message.HasMentionPrefix(_client.CurrentUser, ref argPos)
-				|| message.Author.IsBot)
-				return;
-
-			var context = new SocketCommandContext(_client, message);
-			var commandName = GetCommandNameInInput(message);
-
-			if (HandleInexistantCommand(commandName))
-				await OnInexistantCommandReceived(message);
-			else {
-				var logmsg = LogService.CMD_RECEIVED + Environment.NewLine + LogService.ReadCommandContext(context, commandName);
-				LogService.Info(nameof(OnMessageReceivedAsync), logmsg);
-				await Task.Run(async () => await _command.ExecuteAsync(context, argPos, _services));
-			}
-			return;
-		}
-
-		private bool HandleInexistantCommand(string commandName)
-		{
-			return !CommandDescription.Commands.Any(cmd => cmd.Aliases.Contains(commandName));
-		}
-
-		private string GetCommandNameInInput(SocketUserMessage message)
-		{
-			var commandWithoutCommandMark = message.Content.Remove(0, 1);
-			var splits = commandWithoutCommandMark.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-			return splits[0];
-		}
-
-		private async Task OnInexistantCommandReceived(SocketUserMessage message)
-		{
-			var logMessage = $"{LogService.UNKNOWN_COMMAND} : {message.Content}";
-
-			LogService.Info(nameof(OnInexistantCommandReceived), logMessage);
-			await message.ReplyAsync(LogService.UNKNOWN_COMMAND_HELP);
 		}
 	}
 }
---
+++ Infra/Configuration/CommandMark.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageBot.Infra.Configuration
+{
+	public class CommandMark
+	{
+		public const string COMMAND_MARK = "!";
+	}
+}
