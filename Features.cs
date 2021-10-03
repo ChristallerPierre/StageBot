96a0dd37a204f8a11856688b28c59abe62bc0225
Elemas <pierre.christaller@gmail.com>
2021-10-04 00:55:19 +0200

    features

--- Features.cs
+++ Features.cs
@@ -2,29 +2,20 @@
 {
 	class Features
 	{
-		// todo : new features
-
-		// planification des changements de titres -> lister, ajouter et supprimer une planification
-		// synthèse vocal pour message à diffuser (message, delai entre deux diffusions)
-
-		// messages d'erreur quand commande executée sans paramètre
-
-		// todo : fix time-out sur connexion audio
-		// todo : ajouter gestion des rôles
-		// https://docs.stillu.cc/faq/commands/general.html#how-can-i-restrict-some-of-my-commands-so-only-specific-users-can-execute-them
-		// todo : auto disconnect from voice channels on stop
-		// todo : /commands
-
-		// todo : [RequireBotPermission] pour vérifier que le bot à les droits avant d'exécuter une commande
-
 		/*
-		 * join -> forbidden
-		 * ReplyAsync(erreur)
-		 * log on request received
-		 * start with ongoing scene -> edit title
-		 * edit command -> re-run it
-		 * remove reply on unknown command
-		 * !start <input> when not connected -> connect then start
-		*/
+		 * Nouvelles fonctionnalités :
+		 *	- planification des changements de titres -> lister, ajouter et supprimer une planification
+		 *	- synthèse vocal pour message à diffuser (message, delai entre deux diffusions)
+		 *	- messages d'erreur quand commande executée sans paramètre / en cas d'erreur d'exec
+		 *	- /commands
+		 *	- [RequireBotPermission] pour vérifier que le bot à les droits avant d'exécuter une commande
+		 *	- start with ongoing scene -> edit title
+		 *	- !start <input> when not connected -> connect then start
+		 * 
+		 * Bugfixes :
+		 *	 - fix time-out sur connexion audio
+		 *	 - ne pas log les OpCode quand connecté à un channel vocal
+		 *	 - join -> forbidden
+		 */
 	}
 }
