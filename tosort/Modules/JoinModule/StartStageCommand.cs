68deb3da8ead7fd0d00f50d5c93459a4da5591d4
Elemas <pierre.christaller@gmail.com>
2021-10-04 00:49:24 +0200

    !start : handled case where bot isn't connected to a channel

--- tosort/Modules/JoinModule/StartStageCommand.cs
+++ tosort/Modules/JoinModule/StartStageCommand.cs
@@ -8,38 +8,61 @@ using System.Threading.Tasks;
 
 namespace StageBot.Modules.JoinModule
 {
+	// todo handle Discord.Net.HttpException: The server responded with error 150006: The Stage is already open
+
 	public class StartStageCommand : ModuleBase<SocketCommandContext>
 	{
 		public const string START = "start";
 		public const string CMD_DESC = "!start <titre> pour démarrer la présentation sur la scène.";
+		public const string ERR_PARAM_NB = "Veuillez faire la commande du titre de la conférence";
 
 		[Name(START)]
 		[Command(START, RunMode = RunMode.Async)]
 		[RequireChannel(GuildChannel.BOT_CHAN)]
 		//[RequireUserRole(new[] { GuildRoles.PAMPA_MOD, GuildRoles.PAMPA_RL })]
 		[Summary(CMD_DESC)]
-		public async Task<RuntimeResult> StartStage(string inputTopic)
+		public async Task<RuntimeResult> StartStage()
+		{
+			await ReplyAsync(ERR_PARAM_NB);
+			var result = new CommandResult(CommandError.BadArgCount, LogService.BAD_USAGE);
+			return result;
+		}
+
+		[Name(START)]
+		[Command(START, RunMode = RunMode.Async)]
+		[RequireChannel(GuildChannel.BOT_CHAN)]
+		//[RequireUserRole(new[] { GuildRoles.PAMPA_MOD, GuildRoles.PAMPA_RL })]
+		[Summary(CMD_DESC)]
+		public async Task<RuntimeResult> StartStage([Remainder] string inputTopic)
 		{
 			return await ExecuteCommand(inputTopic);
 		}
 
-		private async Task<RuntimeResult> ExecuteCommand([Remainder] string inputTopic)
+		private async Task<RuntimeResult> ExecuteCommand(string inputTopic)
 		{
 			try {
-				// todo : handle error case
-				// -> topic empty
-				// -> not connected
 				var stageChannel = Context.Guild.GetStageChannel(ContextService.IdStageChannel);
-				await stageChannel.StartStageAsync(inputTopic, StagePrivacyLevel.GuildOnly);
 
-				var message = $"*a démarré la présentation sur la scène {stageChannel.Name}.*";
-				await ReplyAsync(message);
+				if (IsBotConnectedToStage()) {
+					await stageChannel.StartStageAsync(inputTopic, StagePrivacyLevel.GuildOnly);
+					var messageSuccess = $"*a démarré la présentation sur la scène {stageChannel.Name}.*";
+					await ReplyAsync(messageSuccess);
+					return new CommandResult(null, LogService.SUCCESS);
+				} else {
+					var messageFail = $"Le bot doit rejoindre une scène (avec la commande !scene) avant de pouvoir y démarrer une conférence";
+					await ReplyAsync(messageFail);
+					return new CommandResult(CommandError.Unsuccessful, LogService.BAD_USAGE);
+				}
 
-				return new CommandResult(null, LogService.SUCCESS);
 			} catch (Exception ex) {
 				LogService.Error(nameof(StartStageCommand), LogService.ERROR, ex);
 				return new CommandResult(CommandError.Exception, LogService.ERROR);
 			}
+		}
+
+		private bool IsBotConnectedToStage()
+		{
+			return ContextService.IdStageChannel != 0;
 		}
 	}
 }
