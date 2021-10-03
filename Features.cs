b9a34d6a50d16d497809e47e7a6390061801ac8d
Elemas <pierre.christaller@gmail.com>
2021-10-04 01:16:26 +0200

    features

--- Features.cs
+++ Features.cs
@@ -5,6 +5,7 @@
 		/*
 		 * TODO - Nouvelles fonctionnalités :
 		 *	- planification des changements de titres -> lister, ajouter et supprimer une planification
+		 *	- pouvoir lister les titres précédents
 		 *	- synthèse vocal pour message à diffuser (message, delai entre deux diffusions)
 		 *	- messages d'erreur quand commande executée sans paramètre / en cas d'erreur d'exec
 		 *	- /commands
@@ -18,6 +19,8 @@
 		 *	 - ne pas log les OpCode quand connecté à un channel vocal
 		 *	 - join -> forbidden
 		 *	 - crash during night, before 3/10 14h45
+		 *	 - on !exit : System.Threading.Tasks.TaskCanceledException: A task was canceled.
+		 *			at Discord.ConnectionManager.<>c__DisplayClass29_0.<<StartAsync>b__0>d.MoveNext()
 		 */
 	}
 }
